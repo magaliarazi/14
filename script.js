38697366c894fde8410f94f9118140dd27b85d90
magaliarazi <162749320+magaliarazi@users.noreply.github.com>
2024-06-25 10:17:10 -0300

    modified:   index.html 	modified:   script.js 	modified:   style.css

---
+++ script.js
@@ -0,0 +1,77 @@
+let cart = [];
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadCartFromLocalStorage();
+    updateCart();
+});
+
+function addToCart(productId) {
+    const productElement = document.querySelector(`.product[data-id="${productId}"]`);
+    const productName = productElement.getAttribute('data-name');
+    const productPrice = parseFloat(productElement.getAttribute('data-price'));
+    const quantityInput = document.getElementById(`quantity-${productId}`);
+    const quantity = parseInt(quantityInput.value);
+
+    const product = cart.find(item => item.id === productId);
+    if (product) {
+        product.quantity += quantity;
+    } else {
+        cart.push({ id: productId, name: productName, price: productPrice, quantity: quantity });
+    }
+
+    saveCartToLocalStorage();
+    updateCart();
+}
+
+function updateCart() {
+    const cartElement = document.getElementById('cart');
+    cartElement.innerHTML = '';
+
+    if (cart.length === 0) {
+        cartElement.innerHTML = '<p>El carrito está vacío.</p>';
+        updateTotal(0);
+        return;
+    }
+
+    cart.forEach(item => {
+        const itemElement = document.createElement('div');
+        itemElement.className = 'cart-item';
+        itemElement.innerHTML = `
+            <h7>${item.name}</h7>
+            <p>Precio: $${item.price}</p>
+            <p>Cantidad: ${item.quantity}</p>
+            <p>Total: $${(item.price * item.quantity).toFixed(2)}</p>
+            <button onclick="removeFromCart(${item.id})">Eliminar</button>
+        `;
+        cartElement.appendChild(itemElement);
+    });
+
+    const totalPrice = cart.reduce((sum, item) => sum + item.price * item.quantity, 0).toFixed(2);
+    updateTotal(totalPrice);
+}
+
+function removeFromCart(productId) {
+    const productIndex = cart.findIndex(item => item.id === productId);
+    if (productIndex > -1) {
+        cart.splice(productIndex, 1);
+    }
+
+    saveCartToLocalStorage();
+    updateCart();
+}
+
+function updateTotal(totalPrice) {
+    const totalElement = document.getElementById('total');
+    totalElement.innerHTML = `<p>Total de la Compra: $${totalPrice}<p>`;
+}
+
+function saveCartToLocalStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
+
+function loadCartFromLocalStorage() {
+    const savedCart = localStorage.getItem('cart');
+    if (savedCart) {
+        cart = JSON.parse(savedCart);
+    }
+}
